1c4c36e7db6745837f0c2b11ec53eca97bcd9228
nerminajakupovska@gmail.com <nerminajakupovska@gmail.com>
2019-11-15 19:03:30 +0100

    Add project files.

---
+++ Reverse0/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Reverse0
+{
+    class Program
+    {
+        public static string stringReverseString(string str)
+        {
+            char[] chars = str.ToCharArray();
+            for (int i = 0, j = str.Length - 1; i < j; i++, j--)
+            {
+                char c = chars[i];
+                chars[i] = chars[j];
+                chars[j] = c;
+            }
+            return new string(chars);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(stringReverseString("Hello World!"));
+        }
+    }
+}
